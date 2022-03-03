e3422de0f410b22712f23c6e8a138e2219715c0c
Leandro Thiago Facco Galvão Mana <leandro.mana@euemdia.com.br>
2022-03-02 23:35:34 -0300

    20220302 - 2335 - Correcao dos Testes de Rotas.

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,11 +6,15 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    const { user_id } = request.headers;
+    const { user_id } = <{ user_id: string }>request.headers;
 
-    const users = this.listAllUsersUseCase.execute({ user_id });
+    try {
+      const users = this.listAllUsersUseCase.execute({ user_id });
 
-    return response.json(users);
+      return response.json(users);
+    } catch (error) {
+      return response.status(400).json({ error });
+    }
   }
 }
 
